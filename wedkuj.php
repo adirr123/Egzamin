053805942febc12540ca9c91bcae89d8d8c8701c
Adrian Rigga <riggaadrian312@gmail.com>
2023-04-26 11:10:28 +0200

    Style naprawa

--- wedkuj.php
+++ wedkuj.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
-<html lang="pl-PL">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl_1.css">
     <title>Wędkowanie</title>
-    <link rel="stylesheet" href="styl_1.css" />
 </head>
 <body>
-    <header> Portal dla wędkarzy </header>
     
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+
+
+    <aside class="lewy1">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+
+        </ol>
+    </aside>
+
+    <main class="lewy2">
+        <h3>Ryby drapieżne naszych wód</h3>
+    </main>
+
+    <nav class="prawy">
+        <img src="ryba1.jpg" alt="Sum">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </nav>
+
+    <footer>
+        <p>Stronę wykonał: 00000000000</p>
+    </footer>
+
 </body>
 </html>
